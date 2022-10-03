369e1ad484176b0131e983121d6dafabf8c96023
Srimathi-Srinivasan <srimathi1930217@gmail.com>
2022-10-03 21:16:14 +0530

    first commit

---
+++ ecommerceapi/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerceapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext db;
+        public ProductsController(StoreContext _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await db.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await db.Products.FindAsync(id);
+        }
+    }
+}
